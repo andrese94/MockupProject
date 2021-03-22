96e4d787028714e323c4a898da6a071238fd4bf0
SWAndre <81172442+andrese94@users.noreply.github.com>
2021-03-22 15:26:47 +0100

    Add files via upload

---
+++ MockupCreator.jsx
@@ -0,0 +1,324 @@
+
+/*
+Code for Import https://scriptui.joonas.me — (Triple click to select): 
+{"activeId":0,"items":{"item-0":{"id":0,"type":"Dialog","parentId":false,"style":{"enabled":true,"varName":"dlg","windowType":"Dialog","creationProps":{"su1PanelCoordinates":false,"maximizeButton":false,"minimizeButton":false,"independent":false,"closeButton":true,"borderless":false,"resizeable":false},"text":"Mockup Creator","preferredSize":[0,0],"margins":25,"orientation":"column","spacing":10,"alignChildren":["center","top"]}},"item-1":{"id":1,"type":"StaticText","parentId":3,"style":{"enabled":true,"varName":"staticserial","creationProps":{"truncate":"none","multiline":false,"scrolling":false},"softWrap":false,"text":"SW123456","justify":"left","preferredSize":[0,0],"alignment":null,"helpTip":null}},"item-2":{"id":2,"type":"EditText","parentId":3,"style":{"enabled":true,"varName":"editserial","creationProps":{"noecho":false,"readonly":false,"multiline":false,"scrollable":false,"borderless":false,"enterKeySignalsOnChange":false},"softWrap":false,"text":"01","justify":"left","preferredSize":[0,0],"alignment":null,"helpTip":null}},"item-3":{"id":3,"type":"Group","parentId":0,"style":{"enabled":true,"varName":"serialgrp","preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-4":{"id":4,"type":"Panel","parentId":24,"style":{"enabled":true,"varName":"settingspnl2","creationProps":{"borderStyle":"etched","su1PanelCoordinates":false},"text":"Designs Settings","preferredSize":[0,0],"margins":10,"orientation":"column","spacing":10,"alignChildren":["center","top"],"alignment":null},"collapsed":true},"item-5":{"id":5,"type":"Group","parentId":4,"style":{"enabled":true,"varName":"typegrp","preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-6":{"id":6,"type":"DropDownList","parentId":5,"style":{"enabled":true,"varName":"optionstype","text":"DropDownList","listItems":"Option1, Option 2","preferredSize":[0,0],"alignment":null,"selection":0,"helpTip":null}},"item-7":{"id":7,"type":"DropDownList","parentId":5,"style":{"enabled":true,"varName":"colortype","text":"DropDownList","listItems":"Color1,Color2","preferredSize":[0,0],"alignment":null,"selection":0,"helpTip":null}},"item-8":{"id":8,"type":"Group","parentId":4,"style":{"enabled":true,"varName":"capcolorgrp","preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-9":{"id":9,"type":"Checkbox","parentId":8,"style":{"enabled":true,"varName":"capred","text":"Red","preferredSize":[0,0],"alignment":null,"helpTip":null,"checked":false}},"item-10":{"id":10,"type":"Checkbox","parentId":8,"style":{"enabled":true,"varName":"capblue","text":"Blue","preferredSize":[0,0],"alignment":null,"helpTip":null,"checked":false}},"item-11":{"id":11,"type":"Checkbox","parentId":8,"style":{"enabled":true,"varName":"capblack","text":"Black","preferredSize":[0,0],"alignment":null,"helpTip":null,"checked":false}},"item-12":{"id":12,"type":"Panel","parentId":0,"style":{"enabled":true,"varName":"openpnl","creationProps":{"borderStyle":"etched","su1PanelCoordinates":false},"text":"File To Open","preferredSize":[0,0],"margins":10,"orientation":"column","spacing":10,"alignChildren":["left","top"],"alignment":null}},"item-13":{"id":13,"type":"Button","parentId":14,"style":{"enabled":true,"varName":"openbtn","text":"...","justify":"center","preferredSize":[0,0],"alignment":null,"helpTip":null}},"item-14":{"id":14,"type":"Group","parentId":12,"style":{"enabled":true,"varName":"openfilegrp","preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-15":{"id":15,"type":"EditText","parentId":14,"style":{"enabled":true,"varName":"opentext","creationProps":{"noecho":false,"readonly":false,"multiline":false,"scrollable":false,"borderless":false,"enterKeySignalsOnChange":false},"softWrap":false,"text":"Please select a file","justify":"left","preferredSize":[400,0],"alignment":null,"helpTip":null}},"item-16":{"id":16,"type":"DropDownList","parentId":5,"style":{"enabled":true,"varName":"sizetype","text":"DropDownList","listItems":"Size 1,Size 2,Size 3","preferredSize":[0,0],"alignment":null,"selection":0,"helpTip":null}},"item-17":{"id":17,"type":"Panel","parentId":0,"style":{"enabled":true,"varName":"savepnl","creationProps":{"borderStyle":"etched","su1PanelCoordinates":false},"text":"Folder To Save","preferredSize":[0,0],"margins":10,"orientation":"column","spacing":10,"alignChildren":["left","top"],"alignment":null},"collapsed":true},"item-18":{"id":18,"type":"Group","parentId":17,"style":{"enabled":true,"varName":"savegrp","preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-19":{"id":19,"type":"EditText","parentId":18,"style":{"enabled":true,"varName":"savetext","creationProps":{"noecho":false,"readonly":false,"multiline":false,"scrollable":false,"borderless":false,"enterKeySignalsOnChange":false},"softWrap":false,"text":"Select Folder","justify":"left","preferredSize":[400,0],"alignment":null,"helpTip":null}},"item-20":{"id":20,"type":"Button","parentId":18,"style":{"enabled":true,"varName":"savebtn","text":"...","justify":"center","preferredSize":[0,0],"alignment":null,"helpTip":null}},"item-21":{"id":21,"type":"Group","parentId":0,"style":{"enabled":true,"varName":"processgrp","preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-22":{"id":22,"type":"Button","parentId":21,"style":{"enabled":true,"varName":"okbtn","text":"OK","justify":"center","preferredSize":[0,0],"alignment":null,"helpTip":null}},"item-23":{"id":23,"type":"Button","parentId":21,"style":{"enabled":true,"varName":"cancelbtn","text":"CANCEL","justify":"center","preferredSize":[0,0],"alignment":null,"helpTip":null}},"item-24":{"id":24,"type":"Group","parentId":0,"style":{"enabled":true,"varName":"settingsgrp","preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-25":{"id":25,"type":"Panel","parentId":24,"style":{"enabled":true,"varName":"settingspnl1","creationProps":{"borderStyle":"etched","su1PanelCoordinates":false},"text":"Template Color","preferredSize":[0,0],"margins":10,"orientation":"column","spacing":10,"alignChildren":["left","top"],"alignment":null}},"item-26":{"id":26,"type":"Group","parentId":25,"style":{"enabled":true,"varName":"colorgrp","preferredSize":[0,50],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-27":{"id":27,"type":"Button","parentId":26,"style":{"enabled":true,"varName":"colorbtn","text":"Select Color","justify":"center","preferredSize":[0,0],"alignment":null,"helpTip":null}}},"order":[0,3,1,2,12,14,15,13,24,25,26,27,4,5,6,7,16,8,9,10,11,17,18,19,20,21,22,23],"settings":{"importJSON":true,"indentSize":false,"cepExport":false,"includeCSSJS":true,"showDialog":true,"functionWrapper":false,"afterEffectsDockable":false,"itemReferenceList":"None"}}
+*/ 
+
+
+//<---------------------------------- START LAYOUT ---------------------------------->
+
+// DLG
+// ===
+var dlg = new Window("dialog"); 
+    dlg.text = "Mockup Creator"; 
+    dlg.orientation = "column"; 
+    dlg.alignChildren = ["center","top"]; 
+    dlg.spacing = 10; 
+    dlg.margins = 25; 
+
+// SERIALGRP
+// =========
+var serialgrp = dlg.add("group", undefined, {name: "serialgrp"}); 
+    serialgrp.orientation = "row"; 
+    serialgrp.alignChildren = ["left","center"]; 
+    serialgrp.spacing = 10; 
+    serialgrp.margins = 0; 
+
+var staticserial = serialgrp.add("statictext", undefined, undefined, {name: "staticserial"});
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;//January is 0!`
+    var yyyy = today.getFullYear();
+    if (dd<10) {dd='0'+dd};
+    if (mm<10) {mm='0'+mm};
+    if (yyyy>2000) {yyyy=yyyy-"2000"};
+    var SWtoday = 'SW'+(yyyy)+(mm)+dd;
+    staticserial.text = SWtoday; 
+
+var editserial = serialgrp.add('edittext {properties: {name: "editserial"}}'); 
+    editserial.text = "01"; 
+
+// OPENPNL
+// =======
+var openpnl = dlg.add("panel", undefined, undefined, {name: "openpnl"}); 
+    openpnl.text = "File To Open"; 
+    openpnl.orientation = "column"; 
+    openpnl.alignChildren = ["left","top"]; 
+    openpnl.spacing = 10; 
+    openpnl.margins = 10; 
+
+// OPENFILEGRP
+// ===========
+var openfilegrp = openpnl.add("group", undefined, {name: "openfilegrp"}); 
+    openfilegrp.orientation = "row"; 
+    openfilegrp.alignChildren = ["left","center"]; 
+    openfilegrp.spacing = 10; 
+    openfilegrp.margins = 0; 
+
+var opentext = openfilegrp.add('edittext {properties: {name: "opentext"}}'); 
+    opentext.text = "Please select a file"; 
+    opentext.preferredSize.width = 400; 
+
+var openbtn = openfilegrp.add("button", undefined, undefined, {name: "openbtn"}); 
+    openbtn.text = "..."; 
+
+// SETTINGSGRP
+// ===========
+var settingsgrp = dlg.add("group", undefined, {name: "settingsgrp"}); 
+    settingsgrp.orientation = "row"; 
+    settingsgrp.alignChildren = ["left","center"]; 
+    settingsgrp.spacing = 10; 
+    settingsgrp.margins = 0; 
+
+// SETTINGSPNL1
+// ============
+var settingspnl1 = settingsgrp.add("panel", undefined, undefined, {name: "settingspnl1"}); 
+    settingspnl1.text = "Template Color"; 
+    settingspnl1.orientation = "column"; 
+    settingspnl1.alignChildren = ["left","top"]; 
+    settingspnl1.spacing = 10; 
+    settingspnl1.margins = 10; 
+
+// COLORGRP
+// ========
+var colorgrp = settingspnl1.add("group", undefined, {name: "colorgrp"}); 
+    colorgrp.preferredSize.height = 50; 
+    colorgrp.orientation = "row"; 
+    colorgrp.alignChildren = ["left","center"]; 
+    colorgrp.spacing = 10; 
+    colorgrp.margins = 0; 
+    
+
+var colorbtn = colorgrp.add("button", undefined, undefined, {name: "colorbtn"}); 
+    colorbtn.text = "Select Color"; 
+
+// SETTINGSPNL2
+// ============
+var settingspnl2 = settingsgrp.add("panel", undefined, undefined, {name: "settingspnl2"}); 
+    settingspnl2.text = "Designs Settings"; 
+    settingspnl2.orientation = "column"; 
+    settingspnl2.alignChildren = ["center","top"]; 
+    settingspnl2.spacing = 10; 
+    settingspnl2.margins = 10; 
+
+// TYPEGRP
+// =======
+var typegrp = settingspnl2.add("group", undefined, {name: "typegrp"}); 
+    typegrp.orientation = "row"; 
+    typegrp.alignChildren = ["left","center"]; 
+    typegrp.spacing = 10; 
+    typegrp.margins = 0; 
+
+var optionstype_array = ["Option1","Option 2"]; 
+var optionstype = typegrp.add("dropdownlist", undefined, undefined, {name: "optionstype", items: optionstype_array}); 
+    optionstype.selection = 0; 
+
+var colortype_array = ["Color1","Color2"]; 
+var colortype = typegrp.add("dropdownlist", undefined, undefined, {name: "colortype", items: colortype_array}); 
+    colortype.selection = 0; 
+
+var sizetype_array = ["Size 1","Size 2","Size 3"]; 
+var sizetype = typegrp.add("dropdownlist", undefined, undefined, {name: "sizetype", items: sizetype_array}); 
+    sizetype.selection = 0; 
+
+// CAPCOLORGRP
+// ===========
+var capcolorgrp = settingspnl2.add("group", undefined, {name: "capcolorgrp"}); 
+    capcolorgrp.orientation = "row"; 
+    capcolorgrp.alignChildren = ["left","center"]; 
+    capcolorgrp.spacing = 10; 
+    capcolorgrp.margins = 0; 
+
+var capred = capcolorgrp.add("checkbox", undefined, undefined, {name: "capred"}); 
+    capred.text = "Red"; 
+
+var capblue = capcolorgrp.add("checkbox", undefined, undefined, {name: "capblue"}); 
+    capblue.text = "Blue"; 
+
+var capblack = capcolorgrp.add("checkbox", undefined, undefined, {name: "capblack"}); 
+    capblack.text = "Black"; 
+
+// SAVEPNL
+// =======
+var savepnl = dlg.add("panel", undefined, undefined, {name: "savepnl"}); 
+    savepnl.text = "Folder To Save"; 
+    savepnl.orientation = "column"; 
+    savepnl.alignChildren = ["left","top"]; 
+    savepnl.spacing = 10; 
+    savepnl.margins = 10; 
+
+// SAVEGRP
+// =======
+var savegrp = savepnl.add("group", undefined, {name: "savegrp"}); 
+    savegrp.orientation = "row"; 
+    savegrp.alignChildren = ["left","center"]; 
+    savegrp.spacing = 10; 
+    savegrp.margins = 0; 
+
+var savetext = savegrp.add('edittext {properties: {name: "savetext"}}'); 
+    savetext.text = "Select Folder"; 
+    savetext.preferredSize.width = 400; 
+
+var savebtn = savegrp.add("button", undefined, undefined, {name: "savebtn"}); 
+    savebtn.text = "..."; 
+
+// PROCESSGRP
+// ==========
+var processgrp = dlg.add("group", undefined, {name: "processgrp"}); 
+    processgrp.orientation = "row"; 
+    processgrp.alignChildren = ["left","center"]; 
+    processgrp.spacing = 10; 
+    processgrp.margins = 0; 
+
+var okbtn = processgrp.add("button", undefined, undefined, {name: "okbtn"}); 
+    okbtn.text = "OK"; 
+
+var cancelbtn = processgrp.add("button", undefined, undefined, {name: "cancelbtn"}); 
+    cancelbtn.text = "CANCEL"; 
+
+
+
+
+//<---------------------------------- END LAYOUT ---------------------------------->
+
+
+
+
+
+//<---------------------------------- START FUNTIONS ---------------------------------->
+
+// COLOR PICKER
+function getColorpickerColor(){ 
+    if (app.showColorPicker())
+    {
+        return app.foregroundColor
+    }
+    else
+    {
+        return false
+    }
+}; // end of getColor()
+
+// ----------- WORK FUNCTIONS -----------
+
+function createSectors(){ // FROM ACTION
+
+// HERE I CREATE SECTORS ON THE DOCUMENT DEPENDING ON THE SELECTED SIZE TO LATER EXPORT THEM AS PNG
+
+};
+
+function openPSD(){ // FROM ACTION
+
+// HERE I OPEN A PSD DEPENDING ON THE SELECTED SIZE TO LATER UPDATE SMART OBJECTS (THE SECTORS THAT I EXPORTED BEFORE)
+
+};
+
+function changeTextOnPSD(){ // FROM ACTION
+
+// HERE I CHANGE A TEXT LAYER IN ORDER TO UPDATE THE SERIAL NUMBER (staticserial.text + editserial.text)
+
+};
+
+function changeColorLayerOnPSD(){ // FROM ACTION
+
+// HERE I CHANGE THE COLOR FROM MY TEMPLATE (SEND VARIABLE [solid_r, solid_g, solid_b] FROM THE COLORPICKER FUNCTION)
+
+};
+
+function saveAsPNG(Destination,Name) {
+  // Guardar
+  function step1(enabled, withDialog) {
+    if (enabled != undefined && !enabled)
+      return;
+    var dialogMode = (withDialog ? DialogModes.ALL : DialogModes.NO);
+    var desc1 = new ActionDescriptor();
+    var desc2 = new ActionDescriptor();
+    desc2.putEnumerated(cTID('Mthd'), sTID("PNGMethod"), sTID("quick"));
+    desc2.putEnumerated(sTID("PNGInterlaceType"), sTID("PNGInterlaceType"), sTID("PNGInterlaceNone"));
+    desc2.putEnumerated(sTID("PNGFilter"), sTID("PNGFilter"), sTID("PNGFilterAdaptive"));
+    desc2.putInteger(cTID('Cmpr'), 6);
+    desc1.putObject(cTID('As  '), sTID("PNGFormat"), desc2);
+    desc1.putPath(cTID('In  '), new File(Destination+'/'+Name+'.png'));
+    desc1.putInteger(cTID('DocI'), 388);
+    desc1.putBoolean(cTID('Cpy '), true);
+    executeAction(sTID('save'), desc1, dialogMode); // I ASSUME THAT HERE IS ACCTUALLY JUST EXECUTING THE ACTION
+  };
+
+  step1();      // SAVE
+};
+
+//<---------------------------------- END FUNTIONS ---------------------------------->
+
+
+
+
+
+//<---------------------------------- OnClick FUNTIONS ---------------------------------->
+
+// Open explorer to select file and bring name back to opentext field
+openbtn.onClick = function ()  {   
+selectFileOpen = File.openDialog("Please select your template file.","*.*");   
+        if(selectFileOpen != null) opentext.text = decodeURI(selectFileOpen.fsName); 
+}
+
+colorbtn.onClick = function () {
+  var myColor = getColorpickerColor(); 
+
+   
+    var hexToR = function(hex) {
+    var color_r = hex >> 16;
+    var solid_r = color_r / 255;
+    return solid_r;
+
+    };
+    var hexToG = function(hex) {
+    var color_g = hex >> 8 & 0xFF;
+    var solid_g = color_g / 255;
+    return solid_g;
+
+    };
+
+    var hexToB = function(hex) {
+    var color_b = hex & 0xFF;
+    var solid_b = color_b / 255;
+    return solid_b;
+
+    };
+
+    var solid_r = hexToR(parseInt(myColor.rgb.hexValue, 16)); // Maybe I can join all 3 colors in one function 
+    var solid_g = hexToG(parseInt(myColor.rgb.hexValue, 16)); // but don't know how to take out the 3 variables as text with comas and space
+    var solid_b = hexToB(parseInt(myColor.rgb.hexValue, 16)); // return solid_r + ', ' + solid_g + ', ' + solid_b is not working for me
+    colorgrp.graphics.backgroundColor = colorgrp.graphics.newBrush (colorgrp.graphics.BrushType.SOLID_COLOR, [solid_r, solid_g, solid_b]);
+    colorbtn.text = "Change Color";     
+
+    
+};
+
+savebtn.onClick = function ()  {   
+selectFileSave = Folder.selectDialog("Please select your location.","*.*");   
+        if(selectFileSave != null) savebtn.text = decodeURI(selectFileSave.fsName); 
+}
+
+okbtn.onClick = function () {
+
+        dlg.close() //CLOSE DIALOG
+        open (selectFileOpen); //OPEN FIRST FILE
+        FUNCTIONS // HERE I MUST CALL ALL MY WORK FUNCTIONS (CREATESECTORS, OPENPSD, CHANGETEXT AND CHANGECOLOR ONPSD) DEPENDING ON VALUES FROM WINDOWS UI
+
+        // I GUESS I WILL USE A FOR HERE TO SAVE
+        var name = staticserial.text+editserial.text;+capcolor // SO BASSICALLY I NEED TO SAVE EVERY CAP COLOR THAT I SELECTED ON THE UI 
+        saveAsPNG(decodeURI(selectFileSave.fsName),name)
+        
+
+}
+
+cancelbtn.onClick = function () {
+
+        dlg.close() //CLOSE DIALOG
+        
+
+}
+
+//<---------------------------------- END On Click FUNTIONS ---------------------------------->
+
+dlg.show();
